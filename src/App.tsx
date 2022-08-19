e20aedcd36267ebce3f5e91bc8e858e6e4d04cc9
Elona Kajtazi <MY_NAME@example.com>
2022-08-19 17:26:06 +0300

    It's not working YET!

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -44,37 +44,87 @@ function App() {
     "placeholder",
     "search",
   ]);
-  const [mistakes, setMistakes] = useState(5);
-  const [characters, setCharacters] = useState([]);
+  const [maxMistakes, setMaxMistakes] = useState(5);
+  const charactersInWord = getRandomWord().split("");
+  const [characters, setCharacters] = useState(charactersInWord);
+  const [mistakes, setMistakes] = useState(!charactersInWord.length);
+  const [guesses, setGuesses] = useState([]);
   function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
     return words[randomIndex];
   }
-  // function getMistakes() {
-  //   let mistakes = characters.filter(
+
+  function checkIfUserWon() {
+    // are all the letters in state.word in state.characters?
+    for (let char of characters) {
+      if (characters.includes(char)) return false;
+    }
+
+    // We know that every single letter in the word is in the characters array
+    return true;
+
+    // approach 2: using array.every
+    // state.word.split("").every((char) => state.characters.includes(char));
+  }
+  function getMistakes() {
+    let mistakes = characters.filter(
+      (char) => !words.map((word) => word.includes(char))
+    );
+    return mistakes;
+  }
+
+  //Count the letters in state.guessedLetters that ARE NOT in state.word ✅
+  function getMistakeCount() {
+    let mistakes = getMistakes();
+    return mistakes.length;
+  }
+  function checkIfUserLost() {
+    return getMistakeCount() >= maxMistakes;
+  }
+  // function getGuessedLetters() {
+  //   let guessedLetters = characters.filter(
   //     (char) => !words.map((word) => word.includes(char))
   //   );
-  //   return mistakes;
+  //   return guessedLetters;
   // }
-  // function getMistakeCount() {
-  //   let mistakes = getMistakes();
-  //   return mistakes.length;
-  // }
-  // // function getCorrectGuesses() {
-  //   let correctGuesses = characters.filter((char) =>
-  //     state.word.includes(char)
-  //   );
-  //   return correctGuesses;
-  // }
-  const charactersInWord = getRandomWord().split("");
+  useEffect(() => {
+    function handleKeyUp(event) {
+      let guess = event.key.toLowerCase();
+
+      let letters = "abcdefghijklmnopqrstuvwxyz";
+      // GUARD STATEMENTS
+      //1. No sppecial characters should be allowed onluy letters a-z
+      if (!letters.includes(guess)) return;
+
+      //2. No duplicate letters should be allowed
+      if (characters.includes(guess)) return;
+      // //3. If the user lost, don't allow them to guess anymore
+      if (checkIfUserLost()) return;
+      //4. If the user won, don't allow them to guess anymore
+      if (checkIfUserWon()) return;
+      // This only happens if all the guard statements are FALSE
+      // setCharacters([...characters, guess]);
+      // state.characters.push(guess);
+      let charactersCopy = structuredClone(characters);
+      charactersCopy.push(guess);
+      setCharacters(charactersCopy);
+      console.log(charactersCopy);
+    }
+
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [guesses]);
   return (
-    <div className="App">
+    <div
+      className="App"
+    >
+      <h1>REACT VERSION OF THIS GAME</h1>
       <div className="word">
         {charactersInWord.map((char) => (
           <span className="char">_</span>
         ))}
       </div>
-      <div className="mistakes">Wrong guesses: (0)</div>
+      <div className="mistakes">Wrong guesses:({mistakes})</div>
       <div className="streak">Streak: 0</div>
     </div>
   );
