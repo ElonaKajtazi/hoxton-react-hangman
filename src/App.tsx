011ce5d65ccec3247b060fa5a39c9decdc0de59e
Elona Kajtazi <MY_NAME@example.com>
2022-08-19 15:35:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+const [words, setWords] = useState([ "javascript",
+"typescript",
+"react",
+"chair",
+"table",
+"pen",
+"book",
+"mouse",
+"keyboard",
+"monitor",
+"laptop",
+"cellphone",
+"headphones",
+"camera",
+"speaker",
+"microphone",
+"apple",
+"microsoft",
+"google",
+"amazon",
+"facebook",
+"potato",
+"tomato",
+"banana",
+"orange",
+"pear",
+"grape",
+"strawberry",
+"call",
+"text",
+"email",
+"sms",
+"message",
+"map",
+"navigation",
+"location",
+"address",
+"place",
+"placeholder",
+"search",])
+
+  return (
+    <div className="App">
+   
+    </div>
+  )
+}
+
+export default App
