e2649290e25a9192d4df537a29f8240fd5737041
Elona Kajtazi <MY_NAME@example.com>
2022-08-19 15:39:17 +0300

    Got the state for the words and the function...

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,59 @@
-
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-const [words, setWords] = useState([ "javascript",
-"typescript",
-"react",
-"chair",
-"table",
-"pen",
-"book",
-"mouse",
-"keyboard",
-"monitor",
-"laptop",
-"cellphone",
-"headphones",
-"camera",
-"speaker",
-"microphone",
-"apple",
-"microsoft",
-"google",
-"amazon",
-"facebook",
-"potato",
-"tomato",
-"banana",
-"orange",
-"pear",
-"grape",
-"strawberry",
-"call",
-"text",
-"email",
-"sms",
-"message",
-"map",
-"navigation",
-"location",
-"address",
-"place",
-"placeholder",
-"search",])
+  const [words, setWords] = useState([
+    "javascript",
+    "typescript",
+    "react",
+    "chair",
+    "table",
+    "pen",
+    "book",
+    "mouse",
+    "keyboard",
+    "monitor",
+    "laptop",
+    "cellphone",
+    "headphones",
+    "camera",
+    "speaker",
+    "microphone",
+    "apple",
+    "microsoft",
+    "google",
+    "amazon",
+    "facebook",
+    "potato",
+    "tomato",
+    "banana",
+    "orange",
+    "pear",
+    "grape",
+    "strawberry",
+    "call",
+    "text",
+    "email",
+    "sms",
+    "message",
+    "map",
+    "navigation",
+    "location",
+    "address",
+    "place",
+    "placeholder",
+    "search",
+  ]);
+  function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  }
 
   return (
     <div className="App">
-   
+      <h1>hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
